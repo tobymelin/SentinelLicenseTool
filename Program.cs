48441ed5302a19820f0bffb290c46f3f25b61f3d
Toby Melin <n.tobias.melin@gmail.com>
2020-10-26 21:52:42 +0000

    Initial commit for .NET Core 3.1 migration

--- Program.cs
+++ Program.cs
@@ -1,20 +1,3 @@
-﻿/* Sentinel License Query Tool
- * Copyright (C) 2018-2019  Tobias Melin
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +9,12 @@ namespace LicenseManager
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
